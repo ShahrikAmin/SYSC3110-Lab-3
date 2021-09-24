9e9178e4c9127d079b851b2dbae2bc9264e68e2c
Shahrik Amin <shahrikamin2001@gmail.com>
2021-09-24 13:01:16 -0400

    Lab 3 Co

---
+++ src/BuddyInfo.java
@@ -0,0 +1,33 @@
+public class BuddyInfo {
+
+    private String name;
+    private String address;
+    private String phoneNumber;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getAddress() {return this.address; }
+
+    public String getPhoneNumber() { return this.phoneNumber; }
+
+
+    // Default Constructor
+    public BuddyInfo() {
+        this("", "", "");
+    }
+
+    // Constructor
+    public BuddyInfo(String name, String address, String phoneNumber) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public static void main(String[] args) {
+        BuddyInfo BuddyInfo1 = new BuddyInfo("Homer", "", "");
+        System.out.println("Hello" + BuddyInfo1.getName());
+    }
+
+}
